f8f50b942ed74747ce39514bb769b8be90684936
Deadinsidek <dizexen@gmail.com>
2020-06-26 21:18:52 +0300

    Added_shooting

---
+++ Assets/Scripts/Fireball_script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball_script : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+
+
+void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Invoke("DestroySelf", .5f);
+    }
+
+
+void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(80, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
+
