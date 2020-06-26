6c013d76d9fbdd080394b9631c24733a74179d28
Deadinsidek <dizexen@gmail.com>
2020-06-27 01:27:22 +0300

    Added: Camera_following(basic), Shooting(well-done), combined Shooting + Movements = Controller, sound. Fixed: Fatal Errors in console

--- Assets/Scripts/Fireball_script.cs
+++ Assets/Scripts/Fireball_script.cs
@@ -2,33 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Fireball_script : MonoBehaviour
 {
     Rigidbody2D rb2d;
+   private SpriteRenderer spriteRenderer;
+    Animator animator;
 
 
+    [SerializeField]
+    public float fireball_speed = 20;
 
-void Start()
+    [SerializeField]
+    int fireballdamage = 20;
+
+    [SerializeField]
+    float time_to_destroy = 5;
+
+    public void Awake()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
-        Invoke("DestroySelf", .5f);
+        
     }
 
-
-void FixedUpdate()
+    public void StartShoot(bool IsFacingleft)
     {
-        rb2d.velocity = new Vector2(80, 0);
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("ground"))
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        if (IsFacingleft)
         {
-            Destroy(gameObject);
-        }
-    }
-    private void DestroySelf()
-    {
-        Destroy(gameObject);
-    }
-}
+            
+            rb2d.velocity = new Vector2(-fireball_speed, 0);
+            spriteRenderer.flipX = true;
 
+
+        }
+        else if(IsFacingleft=true)
+        {
+            
+            rb2d.velocity = new Vector2(fireball_speed, 0);
+            
+
+        }
+
+        Destroy(gameObject, time_to_destroy);
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Player_controller : MonoBehaviour
+{
+    // Да посмотри ты уже видос про isGrounded, делается за 20 секунд https://www.youtube.com/watch?v=44djqUTg2Sg      30:38
+    // Если не сделаешь до того как я проснусь, то сам сделаю :Evilsmailik:
+    AudioSource audioSrc;
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    [SerializeField]
+    private float speedX = 10f;
+
+    [SerializeField]
+    private float speedY = 10f;
+    
+    //Все что ниже - для выстрелов, не трогай плез <3 
+    public bool IsFacingleft;
+
+    private bool isShooting;
+
+
+    [SerializeField]
+    public float shootingDelay = .5f;
+
+    [SerializeField]
+    Transform fireball_spawn_pos;
+
+    [SerializeField]
+    GameObject fireball;
+
+    
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+
+    }
+
+
+        
+    public void FixedUpdate()
+    {
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            IsFacingleft = false;
+
+            rb2d.velocity = new Vector2(speedX, rb2d.velocity.y);
+
+            spriteRenderer.flipX = true;
+        }
+
+
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            IsFacingleft = true;
+
+            rb2d.velocity = new Vector2(-speedX, rb2d.velocity.y);
+
+            spriteRenderer.flipX = false;
+            
+        }
+        else
+        {
+            
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            animator.Play("idle_anim");
+        }
+
+
+
+
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, speedY);        
+
+        }
+
+
+
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.LeftControl))
+        {
+            if (isShooting) return;
+
+            //animation.Play("shooting")
+            isShooting = true;
+
+            audioSrc.Play();
+
+            GameObject fir = Instantiate(fireball);
+            fir.GetComponent<Fireball_script>().StartShoot(IsFacingleft);
+            fir.transform.position = fireball_spawn_pos.transform.position;
+            Invoke("ResetShoot", shootingDelay);
+
+        }
+
+    }
+    void ResetShoot()
+    {
+        isShooting = false;
+        //animation.Play("Idle")
+
+    }
+
+}
